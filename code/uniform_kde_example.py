aac5e18fcffe3c6d7cffa8266b6c98e6c0ba2a31
mikedelong <mikedelong@outlook.com>
2018-05-17 09:32:29 -0400

    feature: add a third column of values feature: separate out the row and column counts as variables feature: plot the density dataset, which turns out to be the same as the original data.

--- code/uniform_kde_example.py
+++ code/uniform_kde_example.py
@@ -16,24 +16,30 @@ logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
+nrows = 2
+ncols = 3
+figure, ([axes_00, axes_01, axes_02], [axes_10, axes_11, axes_12]) = plt.subplots(nrows=nrows, ncols=ncols,
+                                                                                  figsize=(5 * nrows, 5 * ncols))
+
 data0 = np.random.uniform(0, 1, size=100)
 density0 = gaussian_kde(data0)
 density0.covariance_factor = lambda: .25
 density0._compute_covariance()
 logger.debug(density0.dataset)
-figure, ([axes_00, axes_01], [axes_10, axes_11]) = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
-axes_00.plot(data0, 'k.', markersize=1)
+axes_00.plot(data0, 'r.', markersize=1)
 xs0 = np.linspace(min(data0), max(data0), 100)
-axes_01.plot(xs0, density0(xs0))
+axes_01.plot(xs0, density0(xs0), 'b-')
+axes_02.plot(density0.dataset[0], 'g-')
 
 data1 = np.random.uniform(0, 1, size=100)
 density1 = gaussian_kde(data1)
 density1.covariance_factor = lambda: .25
 density1._compute_covariance()
 logger.debug(density1.dataset)
-axes_10.plot(data1, 'k.', markersize=1)
+axes_10.plot(data1, 'r.', markersize=1)
 xs1 = np.linspace(min(data1), max(data1), 100)
-axes_11.plot(xs1, density1(xs1))
+axes_11.plot(xs1, density1(xs1), 'b-')
+axes_12.plot(density1.dataset[0], 'g-')
 
 plt.show()
 
