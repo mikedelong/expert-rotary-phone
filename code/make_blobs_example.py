6f326e9b602e497055dd8f140f851926c9164e40
mikedelong <mikedelong@outlook.com>
2018-05-18 13:59:23 -0400

    bug fix: fixed the output file path

--- code/make_blobs_example.py
+++ code/make_blobs_example.py
@@ -26,7 +26,7 @@ axes_00.scatter(x0[:, 0], x0[:, 1], c=y0, s=1)
 
 x1, y1 = make_blobs(points, n_features=2, centers=1, random_state=random_state)
 axes_01.scatter(x1[:, 0], x1[:, 1], c='green', s=1)
-output_file = '../output_make_blobs.png'
+output_file = '../output/make_blobs.png'
 logger.debug('saving blob pictures to %s' % output_file)
 plt.savefig(output_file)
 
